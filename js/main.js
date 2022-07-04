b30a7648885b6cd7c1b25b0f5a7804bf415235b0
Roman Kulikov <kulikovroman746@gmail.com>
2022-07-04 12:39:20 +0300

    Доработано добавление скидки

--- js/main.js
+++ js/main.js
@@ -17,7 +17,6 @@ const createCartItem = (item) => {
 
     const oldPrice = item.oldPrice ? item.oldPrice + '₽' : '';
 
-   
     li.innerHTML = `
         <div class="cart-item__left">
             <h3 class="cart-item__title">${item.title}</h3>
@@ -31,24 +30,22 @@ const createCartItem = (item) => {
                 <span class="cart-item__old-price"> ${oldPrice} </span>
                 <span class="price"> ${item.price} ₽ </span>
             </p>
-            <button class="cart-item__remove">
+            <button class="cart-item__remove" data-btn-id="${item.id}">
                 <svg class="itemSvg" width="100%" height="100%" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M8 2C4.682 2 2 4.682 2 8C2 11.318 4.682 14 8 14C11.318 14 14 11.318 14 8C14 4.682 11.318 2 8 2ZM8 12.8C5.354 12.8 3.2 10.646 3.2 8C3.2 5.354 5.354 3.2 8 3.2C10.646 3.2 12.8 5.354 12.8 8C12.8 10.646 10.646 12.8 8 12.8ZM10.154 5L8 7.154L5.846 5L5 5.846L7.154 8L5 10.154L5.846 11L8 8.846L10.154 11L11 10.154L8.846 8L11 5.846L10.154 5Z" fill="#B0B0B0"/>
                 </svg>
             </button>
         </div>
     `;
-    
+    li.querySelector('.cart-item__remove').addEventListener('click', deleteCartItem);
 }
 
 const updatePrecheck = () => {
     const precheck = document.querySelector('.products__precheck');
     const count = cart.getCountAll();
     const price = cart.getSum();
-    const discount = cart.getDiscount();
+    const discount = cart.getDiscountSum();
     const priceNotDiscount = price + discount;
-
-    console.log(price, discount);
 
     precheck.innerHTML = `
         <h1 class="precheck__title">Ваши товары</h1>
@@ -72,15 +69,18 @@ const updatePrecheck = () => {
 
 const checkLocalCart = () => {
     const data = cart.getData();
+    const discount = cart.getDiscount();
+
+    if (discount) {
+        document.querySelector('.input-discount').value = discount;
+    }
 
     if (data.length > 0) {
         data.forEach(item => createCartItem(item));
     } else {
-        document.querySelector('.cart-block__message').classList.toggle('disable');
+        document.querySelector('.cart-block__message').classList.remove('disable');
     }
 }
-
-
 
 const addCartItem = (event) => {
     event.preventDefault();
@@ -94,28 +94,65 @@ const addCartItem = (event) => {
         errors.forEach(error => document.querySelector('.error-product').append(error));
         return;
     }
-
+    
+   
     cart.add({
         "id": id.value,
         "title": title.value,
         "price": price.value,
         "count": 1
     });
-    
+
+    if (cart.getDiscount()) {
+        cart.setDiscount();
+    }
+
     createCartItem(cart.getById(id.value));
+    updatePrecheck();
 
     [id, title, price].forEach(input => input.value = '');
-    document.querySelector('.cart-block__message').classList.remove('disable');
+    document.querySelector('.cart-block__message').classList.add('disable');
+}
+
+const deleteCartItem = (event) => {
+    event.preventDefault();
+    let id = event.target.dataset.btnId;
+    document.querySelector(`[data-item-id="${id}"]`).remove();
+    if (cart.remove(id).length == 0) {
+        document.querySelector('.cart-block__message').classList.remove('disable');
+    }
+}
+
+const setDiscount = (event) => {
+    event.preventDefault();
+    const discount = document.querySelector('#input-discount');
+    if (event.target.classList.contains('form-discount__remove')) {
+        cart.setDiscount(0);
+        discount.value = '';
+    } else {
+        let errors = validate.test([[discount, 'num']]);
+
+        if (errors.length != 0) {
+            errors.forEach(error => document.querySelector('.error-discount').append(error));
+            return;
+        }
+
+        cart.setDiscount(Number(discount.value));
+    }
+    updatePrecheck();
+    checkLocalCart();
 }
 
 
-// cart.setDiscount(10)
-
 checkLocalCart();
 updatePrecheck();
+
 validate.setEvent(document.querySelectorAll('.form__item-input'));
-document.querySelector('.form-product__add').addEventListener('click', addCartItem)
+document.querySelector('.form-product__add').addEventListener('click', addCartItem);
+document.querySelector('.form-discount__add').addEventListener('click', setDiscount);
+document.querySelector('.form-discount__remove').addEventListener('click', setDiscount);
+document.querySelectorAll('.cart-item__remove').forEach(btn => btn.addEventListener('click', deleteCartItem));
 
 
-
+console.log(cart.getDiscount());
 
